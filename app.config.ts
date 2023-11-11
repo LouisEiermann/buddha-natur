0bf9a8ed3b86a6cacacb848975627738cf99cd0c
LouisEiermann <64272749+LouisEiermann@users.noreply.github.com>
2023-11-11 22:39:35 +0100

    logos

--- app.config.ts
+++ app.config.ts
@@ -25,16 +25,18 @@ export default defineAppConfig({
         enabled: false,
 
         // our github repository
-        repository: 'https://www.github.com/nuxt-themes/alpine',
+        repository: '',
 
-        text: 'Buddhanatur',
+        text: '',
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Folg mir' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: '' // string that will be displayed in the footer (leave empty or delete to disable)
     },
 
     socials: {
+      twitter: '',
+      instagram: '',
     },
 
     form: {
