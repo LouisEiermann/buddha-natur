88d1cae60a82488c5092fa9fa1fa24dc2c23d66d
Louis Eiermann <louis.eiermann@gmail.com>
2023-11-11 19:09:14 +0100

    favicon

--- app.config.ts
+++ app.config.ts
@@ -37,5 +37,7 @@ export default defineAppConfig({
     form: {
       successMessage: 'Message sent. Thank you!'
     }
-  }
+  },
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
 })
